f081d6e9db9a56dd92be3ac17e2b7fc189f3d704
main <main@mail.ru>
2020-10-16 13:43:55 +0300

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'test';
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient){}
+  currentCourse:string
+  indexUrl:number = 0
+  private url = ['https://www.cbr-xml-daily.ru/daily_utf8.xml','https://www.cbr-xml-daily.ru/daily_json.js']
+
+  ngOnInit(){
+    this.startInterval()
+  }
+  startInterval(){
+    this.getCourses()
+    setInterval(this.getCourses,10000)
+  }
+  getCourses = ()=>{
+      this.http.get(this.url[this.indexUrl]).subscribe(response =>{
+        console.log( this.url[this.indexUrl])
+        this.currentCourse = response.Valute.EUR.Value
+        console.log( this.currentCourse);  
+      },error =>{
+          if(this.indexUrl < this.url.length +1){
+            ++this.indexUrl
+          }else{
+            this.indexUrl = 0
+          }
+          this.getCourses();
+      })
+    }
 }
